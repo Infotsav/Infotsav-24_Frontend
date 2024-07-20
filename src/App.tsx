b90329b64d6f7e802aaffadeb7efd268ad2d5b93
Aditya <adityakrsingh2604@gmail.com>
2024-07-21 00:24:35 +0530

    add: fonts ttf, routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
-// import React from 'react'
+// import React from 'react';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import MainLayout from './MainLayout';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import ContactUs from './Pages/ContactUs';
+import Events from './Pages/Events';
+import Login from './Pages/Login';
+import MeetTheTeam from './Pages/MeetTheTeam';
+import Sponsors from './Pages/Sponsors';
+import NotFound from './Pages/NotFound';
+
+
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <Router>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<ContactUs />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/register" element={<Login />} />
+          <Route path="/meet-the-team" element={<MeetTheTeam />} />
+          <Route path="/sponsors" element={<Sponsors />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
