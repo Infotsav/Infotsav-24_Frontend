033fffbe7acfc8e1ca9bee905f53f0ce27bfc6f5
Divya Agrawal <divyaagrawal0747@gmail.com>
2024-08-04 10:49:28 +0530

    add: On Home Page: Intro Section and Past Sponsors

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,10 +1,14 @@
 // import React from 'react'
 import Lannding from '../Components/Home/Landing'
+import Intro from '../Components/Home/Intro'
+import PastSponsors from '../Components/Home/PastSponsors'
 
 function Home() {
     return (
         <div>
             <Lannding />
+            <Intro/>
+            <PastSponsors/>            
         </div>
     )
 }
---
+++ src/constants/PastSponsors.js
@@ -0,0 +1,76 @@
+import CK from "../assets/PastSponsors/CK.png";
+import CodeChef from "../assets/PastSponsors/CodeChef.png";
+import DainikBhaskar from "../assets/PastSponsors/Dainik_Bhaskar.png";
+import DigitalOcean from "../assets/PastSponsors/Digital_Ocean.png";
+import FabHotels from "../assets/PastSponsors/fabHotels.jpg";
+import FestPav from "../assets/PastSponsors/FestPav.png";
+import HackSociety from "../assets/PastSponsors/Hack_Society.png";
+import HackerEarth from '../assets/PastSponsors/HackerEarth.png';
+import InkMonk from "../assets/PastSponsors/InkMonk.png";
+import KnowAFest from "../assets/PastSponsors/KnowAFest.png";
+import MyFM from "../assets/PastSponsors/MyFM.png";
+import PayTM from "../assets/PastSponsors/PayTM.png";
+import Swiggy from "../assets/PastSponsors/Swiggy.png";
+import TCF from "../assets/PastSponsors/The_College_Fever.png";
+
+export const pastSponsors1 = [
+    {
+        name: "CK",
+        src: CK
+    },
+    {
+        name: "CodeChef",
+        src: CodeChef
+    },
+    {
+        name: "Dainik Bhaskar",
+        src: DainikBhaskar
+    },
+    {
+        name: "Digital Ocean",
+        src: DigitalOcean
+    },
+    {
+        name: "FabHotels",
+        src: FabHotels
+    },
+    {
+        name: "FestPav",
+        src: FestPav
+    },
+    {
+        name: "Hack Society",
+        src: HackSociety
+    }
+];
+
+export const pastSponsors2 = [
+    {
+        name: "HackerEarth",
+        src: HackerEarth
+    },
+    {
+        name: "InkMonk",
+        src: InkMonk
+    },
+    {
+        name: "KnowAFest",
+        src: KnowAFest
+    },
+    {
+        name: "MyFM",
+        src: MyFM
+    },
+    {
+        name: "PayTM",
+        src: PayTM
+    },
+    {
+        name: "Swiggy",
+        src: Swiggy
+    },
+    {
+        name: "The College Fever",
+        src: TCF
+    }
+];
