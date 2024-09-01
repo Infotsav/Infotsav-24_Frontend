21a63f6eeff84911ea7188f98b9f846ae2237e6a
Infotsav <infotsav@iiitm.ac.in>
2024-09-02 03:04:06 +0530

    Update MakingEventCard.tsx

--- src/Components/EventDescription/MakingEventCard.tsx
+++ src/Components/EventDescription/MakingEventCard.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useParams} from "react-router-dom";
+import { useParams,Link} from "react-router-dom";
 // import AboutTab from "./AboutTab";
 // import VenueTab from "./VenueTab";
 // import TimeTab from "./TimeTab";
